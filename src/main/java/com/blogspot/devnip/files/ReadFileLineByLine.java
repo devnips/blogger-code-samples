1ee01cbaae90e149dd7546c4974c08b92ee3c363
Anubhav Shukla <ashukla@fispan.com>
2020-09-07 00:44:45 -0700

    RRead file line by line

---
+++ src/main/java/com/blogspot/devnip/files/ReadFileLineByLine.java
@@ -0,0 +1,37 @@
+package com.blogspot.devnip.files;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Read file line by line.
+ */
+public class ReadFileLineByLine
+{
+
+    public static String read()
+    {
+        String fileName = "/multiline-text-file.txt";
+
+        InputStream is = ReadFileLineByLine.class.getResourceAsStream(fileName);
+
+        StringBuilder resultStringBuilder = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is)))
+        {
+            String line;
+            while ((line = br.readLine()) != null)
+            {
+                resultStringBuilder.append(line)
+                        .append("\n");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return resultStringBuilder.toString();
+    }
+}
