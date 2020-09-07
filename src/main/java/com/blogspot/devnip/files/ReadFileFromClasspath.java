a910d71f1f6028b84d736b89c24fbbe92188816b
Anubhav Shukla <ashukla@fispan.com>
2020-09-07 00:24:49 -0700

    Read file from classpath

---
+++ src/main/java/com/blogspot/devnip/files/ReadFileFromClasspath.java
@@ -0,0 +1,25 @@
+package com.blogspot.devnip.files;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+/**
+ * Read file content using plain Java 8
+ */
+public class ReadFileFromClasspath
+{
+    public static void main(String[] args)
+    {
+        InputStream is = ReadFileFromClasspath.class.getResourceAsStream("/sample-text-file.txt");
+
+        String fileContent = new BufferedReader(
+                new InputStreamReader(is, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        System.out.println(fileContent);
+    }
+}
